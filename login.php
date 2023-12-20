c1dfad8795733c77e38d73bc882d313554f44a14
dev-rabin <robinmandhotia@gmail.com>
2023-12-20 21:33:46 +0530

    ui-change

--- login.php
+++ login.php
@@ -55,18 +55,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 <div class="container my-4 ">
         <h1 class="text-center">Login here</h1>
-        <form action="login.php" method="post">
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
-
+        <form action="signup.php" method="post">
+            <div class="row d-flex justify-content-around">
+                <div class="col-6">
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password">
+                </div>
+                <div id="emailHelp" class="form-text my-2">Please enter both passwords same</div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <!-- <div> <img class="img-fluid col-6" src="./images/image.jpg" alt="sign-up image" srcset="" height="400px" width = "500px"></div> -->
+                </div>
+                
             </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" name="password">
-            </div>
-            <div id="emailHelp" class="form-text my-2">Not registered ? <span><a href="./signup.php">Click here</a></span></div>
-            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
 </body>
--- signup.php
+++ signup.php
@@ -6,25 +6,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $password = $_POST["password"];
     $cpassword = $_POST["cpassword"];
-   $exists = false;
+    $exists = false;
     // Check if passwords match
-    if(($password == $cpassword && $exists==false)){
+    if (($password == $cpassword && $exists == false)) {
         $sql = "INSERT INTO `users` ( `email`, `password`, `time`) VALUES ('$email', '$password', current_timestamp())";
 
         $result = mysqli_query($conn, $sql);
-        if ($result){
+        if ($result) {
             $success = true;
         }
-    }
-    else{
+    } else {
         $failed = "Passwords do not match";
     }
-    
+
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -32,6 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
     <?php require "./partials/_nav.php" ?>
     <?php
@@ -55,21 +56,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="container my-4 ">
         <h1 class="text-center">Register here</h1>
         <form action="signup.php" method="post">
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
-
+            <div class="row d-flex justify-content-around">
+                <div class="col-6">
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+                    <input type="password" class="form-control" id="password" name="cpassword">
+                </div>
+                <div id="emailHelp" class="form-text my-2">Please enter both passwords same</div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <!-- <div> <img class="img-fluid col-6" src="./images/image.jpg" alt="sign-up image" srcset="" height="400px" width = "500px"></div> -->
+                </div>
+                
             </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" name="password">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-                <input type="password" class="form-control" id="password" name="cpassword">
-            </div>
-            <div id="emailHelp" class="form-text my-2">Please enter both passwords same</div>
-            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
 </body>
