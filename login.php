8c96178b5a5e20cbe6c0a05f4549c2b7bc0e710e
dev-rabin <robinmandhotia@gmail.com>
2023-12-20 21:07:47 +0530

    login-completed

---
+++ login.php
@@ -0,0 +1,73 @@
+<?php
+include("partials/_dbConnect.php");
+$login = false;
+$showError = false;
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "select * from users where email='$email' AND password = '$password'";
+    $result = mysqli_query($conn,$sql);
+    $num = mysqli_num_rows($result);
+    if($num == 1){
+        $login = true;
+    }
+    else {
+        $showError= ("Invalid Credentials");
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <!-- Navabar -->
+<?php require "./partials/_nav.php" ?>
+
+    <!-- Alert -->
+    <?php 
+    if($login){
+        echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Login Successfully!</strong> Now you are logged in.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+       </div>';
+    }
+
+   else if($showError){
+        echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>Fill properly!</strong> ' . $showError . '.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+       </div>';
+    }
+
+    ?>
+
+<div class="container my-4 ">
+        <h1 class="text-center">Login here</h1>
+        <form action="login.php" method="post">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <div id="emailHelp" class="form-text my-2">Not registered ? <span><a href="./signup.php">Click here</a></span></div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
--- signup.php
+++ signup.php
@@ -6,10 +6,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $password = $_POST["password"];
     $cpassword = $_POST["cpassword"];
-   
-   
+   $exists = false;
     // Check if passwords match
-    if(($password == $cpassword)){
+    if(($password == $cpassword && $exists==false)){
         $sql = "INSERT INTO `users` ( `email`, `password`, `time`) VALUES ('$email', '$password', current_timestamp())";
 
         $result = mysqli_query($conn, $sql);
@@ -26,7 +25,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -34,7 +32,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-
 <body>
     <?php require "./partials/_nav.php" ?>
     <?php
